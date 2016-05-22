5cc1eba9e8acf6ad8b697170fdcfa3df1ba19f30
Emile Badenhorst <MyvarHD@gmail.com>
2016-05-22 19:22:30 +0200

    added HtmlParser unit tests

--- WebBrowser.Net/RenderingTests/HtmlParserTests.cs
+++ WebBrowser.Net/RenderingTests/HtmlParserTests.cs
@@ -30,5 +30,34 @@ namespace RenderingTests
         {
             Assert.AreEqual(new HtmlParser("<root test=fudge></root>").Root.InnerAttributes, "test=fudge");
         }
+
+        [TestMethod]
+        public void RootElementAttributeNoStringParse()
+        {
+            var x = new HtmlParser("<root test=fudge name=\"lol\"></root>");
+            Assert.AreEqual(x.Root.Attributes["test"], "fudge");
+        }
+
+        [TestMethod]
+        public void RootElementAttributeStringParse()
+        {
+            var x = new HtmlParser("<root test=fudge name=\"lol\"></root>");
+            Assert.AreEqual(x.Root.Attributes["name"], "lol");
+        }
+
+        [TestMethod]
+        public void NestedElementParse()
+        {
+            var x = new HtmlParser("<root test=fudge name=\"lol\"><head></head></root>");
+            Assert.AreEqual(x.Root.Children[0].Name, "head");
+        }
+
+        [TestMethod]
+        public void NestedElementCountParse()
+        {
+            var x = new HtmlParser("<root test=fudge name=\"lol\"><head></head><body></body></root>");
+            Assert.AreEqual(x.Root.Children.Count, 2);
+        }
+
     }
 }
